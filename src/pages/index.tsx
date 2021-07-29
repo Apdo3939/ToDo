96ba0bf57e5bec4de6afddc1596e8ea9d5e1a9fe
Apdo3939 <apdo3939@hotmail.com>
2021-07-29 17:48:16 -0300

    Add typeScript and tailwind css

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+
+export default function Home() {
+  return (
+    <div className={`flex flex-col justify-center items-center 
+    bg-gradient-to-tr from-blue-900 to-red-300 h-screen`}>
+      <span >Hello, world!</span>
+      <span >Hello, world!</span>
+      <span >Hello, world!</span>
+    </div>
+  )
+}
