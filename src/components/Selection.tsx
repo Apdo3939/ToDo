7587588b0d4f810b3bde13253c50c7a22155c1a0
Apdo3939 <apdo3939@hotmail.com>
2021-07-31 13:03:19 -0300

    Add component selection

---
+++ src/components/Selection.tsx
@@ -0,0 +1,18 @@
+interface SelectionProps {
+    checked: boolean
+}
+
+export default function Selection(props: SelectionProps) {
+
+    const gradient = props.checked ?
+        'bg-gradient-to-br from-blue-300 to-blue-900' : 'bg-gray-300'
+
+    return (
+        <div className={
+            `h-7 w-7 flex items-center justify-center border rounded-full cursor-pointer m-2
+            ${gradient} `
+        }>
+            {props.checked ? <span>x</span> : ''}
+        </div>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,14 @@
-import ToDo from "../model/ToDo"
-import toDoMock from "../data/mock"
+
+import Selection from "../components/Selection"
 
 export default function Home() {
 
-  let tasks = toDoMock
-  //tasks = tasks.filterRemove()
-  //tasks = tasks.filterActiveTasks()
-  //tasks = tasks.filterCompletedTasks()
-  //tasks = tasks.removeCompletedTasks()
-  tasks = tasks.addTask(ToDo.activetask(5,'Jogar video games após a faculdade'))
-  const taskUpdate = tasks.itens[2]
-  tasks = tasks.updateTask(taskUpdate.changeStatus())
   
-  function renderListTasks() {
-    return tasks.itens.map(task => {
-      return (
-        <div key={task.id}>
-          <span >{task.id} </span>
-          <span > {task.description}</span>
-          <span > {task.completed ? 'Completada' : 'Ativa'}</span>
-        </div>
-      )
-    })
-  }
-
   return (
     <div className={`flex flex-col justify-center items-center h-screen
     bg-gradient-to-tr from-blue-900 to-red-300 `}>
-      {renderListTasks()}
+      <Selection checked={true} />
+      <Selection checked={false} />
     </div>
 
   )
